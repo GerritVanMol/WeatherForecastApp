19ee00bf778276f4705433f2dbb6544311a6c3e0
Gerrit Van Mol <gerrit.vanmol@student.odisee.be>
2023-09-26 17:08:15 +0200

    Display basic data and add auto refresh to update every 24h

--- WeatherForecastApp/WeatherForecastApp/Pages/index.razor.cs
+++ WeatherForecastApp/WeatherForecastApp/Pages/index.razor.cs
@@ -1,19 +1,53 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Formats.Asn1;
-using System.Globalization;
-using System.Threading;
-using System.Threading.Tasks;
-using WeatherForecastApp.Models;
 using WeatherForecastApp.ViewModels;
-using Microsoft.JSInterop;
-using System.Text;
-using Microsoft.AspNetCore.Mvc;
+
 
 namespace WeatherForecastApp.Pages
 {
-    public class IndexBase : ComponentBase
+    public class IndexBase : ComponentBase, IDisposable
     {
 
+        protected ClimateViewModel viewModel = new ClimateViewModel();
+        protected string updateTime;
+        private CancellationTokenSource cancellationTokenSource;
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadWeatherDataAndScheduleUpdateAsync();
+        }
+
+        private async Task LoadWeatherDataAndScheduleUpdateAsync()
+        {
+            try
+            {
+                await viewModel.LoadWeatherDataAsync();
+                updateTime =DateTime.UtcNow.AddHours(2).ToString("dddd, dd MMMM yyyy HH:mm:ss");
+                ScheduleAutoUpdate();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private void ScheduleAutoUpdate()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+
+            Task.Run(async () =>
+            {
+                while (!cancellationTokenSource.Token.IsCancellationRequested)
+                {
+                    await Task.Delay(TimeSpan.FromHours(24), cancellationTokenSource.Token);
+                    await InvokeAsync(LoadWeatherDataAndScheduleUpdateAsync);
+                    StateHasChanged();
+                }
+            }, cancellationTokenSource.Token);
+        }
+
+        public void Dispose()
+        {
+            cancellationTokenSource?.Cancel();
+            cancellationTokenSource?.Dispose();
+        }
     }
 }
