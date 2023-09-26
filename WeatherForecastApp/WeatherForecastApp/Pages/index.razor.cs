2245ac07e03b08f4a268fabc733609ac77b13dd7
Gerrit Van Mol <gerrit.vanmol@student.odisee.be>
2023-09-26 16:56:27 +0200

    Fetch api data and add base class to index.razor page

---
+++ WeatherForecastApp/WeatherForecastApp/Pages/index.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using WeatherForecastApp.Models;
+using WeatherForecastApp.ViewModels;
+using Microsoft.JSInterop;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecastApp.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+
+    }
+}
--- WeatherForecastApp/WeatherForecastApp/ViewModels/ClimateViewModel.cs
+++ WeatherForecastApp/WeatherForecastApp/ViewModels/ClimateViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace WeatherForecastApp.ViewModels
+﻿using System.Text.Json;
+using WeatherForecastApp.Models;
+
+namespace WeatherForecastApp.ViewModels
 {
     public class ClimateViewModel
     {
+        public ClimateDataPoints? ClimateData { get; private set; }
+        public async Task LoadWeatherDataAsync()
+        {
+            string apiUrl = $"https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,relativehumidity_2m,windspeed_10m&past_days=30";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(apiUrl);
+            var climateData = JsonSerializer.Deserialize<ClimateDataPoints>(response, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            ClimateData = climateData;
+        }
     }
 }
